90357f4cf5de482948d55c5ebff83ff966f19366
Ayus Mohanty <mail2.ayusm@gmail.com>
2020-07-06 21:36:49 +0530

    Updates

---
+++ precache-manifest.7c0d17d5631cb40adff8c8b15afa9cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b34052eff398cf9455e8acf76c0070e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6cb1fa3ca2225d282571",
+    "url": "./static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "383e8a6279e7c8cc5ebb",
+    "url": "./static/css/main.e9b86a69.chunk.css"
+  },
+  {
+    "revision": "6cb1fa3ca2225d282571",
+    "url": "./static/js/2.912ab82f.chunk.js"
+  },
+  {
+    "revision": "a929e4c35621369695e0145e8abf42af",
+    "url": "./static/js/2.912ab82f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383e8a6279e7c8cc5ebb",
+    "url": "./static/js/main.aa450dd9.chunk.js"
+  },
+  {
+    "revision": "1cc6004156968a70cbbf",
+    "url": "./static/js/runtime-main.251af40b.js"
+  }
+]);
